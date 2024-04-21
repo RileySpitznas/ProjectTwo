e4b14bf2d50784ae58fb35b751f9800916579652
RileySpitznas <156121383+RileySpitznas@users.noreply.github.com>
2024-04-21 14:45:31 -0400

    project 2 check in 2
    check in

--- src/playList.js
+++ src/playList.js
@@ -18,12 +18,101 @@ export class PlayList extends LitElement {
       :host {
         display: block;
       }
+
+      .topRow {
+        display: flex;
+        align-items: center;
+        justify-content: space-between; 
+        }
+      .backdrop {
+        margin: 8px;
+        padding: 8px;
+        width:500px;
+        height:500px;
+        background-color:green;
+        color: black;
+        position: relative;
+          }
+       .closeButton {
+        margin: 8px;
+        display: flex;
+        justify-content: right
+          }
+        .description {
+        align-self: center;
+        display: flex;
+        justify-content: center;
+         }
+         .imageLoop {
+        margin: 8px;
+        display: flex;
+        justify-content: space-between;  
+        position: absolute;
+        bottom: 0;
+        width: 95%;
+         }
+         .slide-image {
+        width: 100%;
+        min-height: 200px;
+        height: auto;
+        max-height: 400px;
+        padding: 10px;
+        margin: 0 auto;
+         }
+        #pic {
+        position: absolute;
+        left: 25%;
+        top: 15%;
+        width: 250px;
+        height: 250px;
+        }
     `;
   }
 
+  rightClick()
+  {
+   console.log("hi")
+   if(this.imageNumber < this.totalImageNumber-1)
+   this.imageNumber = this.imageNumber+1;
+ else
+ {
+   this.imageNumber = 0;
+ }
+   this.requestUpdate();
+  }
+  leftClick()
+  {
+   if(this.imageNumber > 0)
+   this.imageNumber = this.imageNumber-1;
+ else
+ {
+   this.imageNumber = this.totalImageNumber-1;
+ }
+   this.requestUpdate();
+  }
+
+
 
   render() {
-    return html`<div>${this.title}</div>`;
+
+    let prevIndex = this.imageNumber - 1;
+    let nextIndex = this.imageNumber + 1;
+    if(prevIndex < 0)
+    {
+      prevIndex = (prevIndex + this.image.length) % (this.image.length);
+    }
+    if(nextIndex >= this.totalImageNumber)
+    {
+      nextIndex = (nextIndex) % (this.image.length);
+    }
+
+
+    return html`
+      
+      <div>${this.title}</div> //idk about this line
+
+
+      `;
   }
 
 
